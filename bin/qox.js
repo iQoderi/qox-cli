676d3f43a1077b97bd3436c5a202462cbe40bccb
Qoder2016 <1621532838@qq.com>
2018-05-10 17:59:05 +0800

    feat: add base commands

---
+++ bin/qox.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+const updateNotifier = require('update-notifier');
+const chalk = require('chalk');
+const semver = require('semver');
+const cli = require('commander');
+const colors = require('colors');
+const pkg = require('../package.json');
+
+const CMD_INIT = require('../src/actions/init');
+const CMD_DEV = require('../src/actions/dev');
+const CMD_BUILD = require('../src/actions/build');
+const CMD_PUBLISH = require('../src/actions/publish');
+const CMD_LINT = require('../src/actions/init');
+
+// Update notifications
+updateNotifier({pkg}).notify();
+
+// Check node version
+if (!semver.satisfies(process.version, '>=6')) {
+  const message = 'You are currently running Node.js ' +
+    chalk.red(process.version) + '.\n' +
+    '\n' +
+    'qox-cli runs on Node 6.0 or newer. There are several ways to ' +
+    'upgrade Node.js depending on your preference.\n' +
+    '\n' +
+    'nvm:       nvm install node && nvm alias default node\n' +
+    'Homebrew:  brew install node\n' +
+    'Installer: download the Mac .pkg from https://nodejs.org/\n';
+
+  console.log(message);
+  process.exit(1);
+}
+
+const commands = ['init', 'build', 'dev', 'lint', 'build', 'publish'];
+
+cli
+  .version(pkg.version)
+  .usage('[command]'.green)
+  .description(pkg.description.green);
+
+// register commands
+cli
+  .command('init [ProjectName]')
+  .description('init Project'.green)
+  .action(CMD_INIT);
+
+
+cli
+  .command('dev')
+  .description('start dev'.green)
+  .action(CMD_DEV);
+
+cli
+  .command('build')
+  .description('build project'.green)
+  .action(CMD_BUILD);
+
+cli
+  .command('publish')
+  .description('build and publish project to cdn'.green)
+  .action(CMD_PUBLISH)
+
+cli
+  .command('lint')
+  .description('lint code'.green)
+  .action(CMD_LINT);
+
+// help
+cli
+  .command('help')
+  .description('check help infomation'.green)
+  .action(() => cli.help());
+
+cli.parse(process.argv);
+
+if (!cli.args.length || !process.argv[2] || !commands.includes(process.argv[2])) {
+  cli.help();
+}
