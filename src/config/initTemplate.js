5dff29c6de7bbd2c81509ebf6709b9400d4db0bb
Qoder2016 <1621532838@qq.com>
2018-05-12 19:53:56 +0800

    feat: add initTemplate

---
+++ src/config/initTemplate.js
@@ -0,0 +1,7 @@
+const rax = 'A Rax Component';
+const react = 'A React Component';
+
+module.exports = {
+  [rax]: 'iQoderi/qox-rax-component-template',
+  [react]: 'iQoderi/qox-rax-component-template'
+};
