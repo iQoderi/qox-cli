cb94c550ba879415182e7742cf326decae7ad7b0
Qoder2016 <1621532838@qq.com>
2018-05-13 02:59:34 +0800

    完成build 命令

--- src/actions/build.js
+++ src/actions/build.js
@@ -1,1 +1,29 @@
-module.exports = function() {};
+const fs = require('fs');
+const colors = require('colors');
+const util = require('../lib/utils');
+const glob = require('glob');
+const { exec } = require('child_process');
+const opn = require('opn');
+
+const { preInstall, printCommandLog } = util;
+const { log, error } = util.msg;
+
+module.exports = function() {
+  const workpath = process.cwd();
+  const qoxJson = `${workpath}/qox.json`;
+  const builderPath = '../builder';
+
+  if (fs.existsSync(qoxJson)) {
+    const { type, yarn, builder } = require(qoxJson);
+
+    printCommandLog();
+    preInstall(yarn);
+
+    const useBuilder = require(`${builderPath}/${builder}/build`);
+
+    exec(`node ${useBuilder}`);
+
+  } else {
+    error(`Can not find ${'qox.json'.bold} in current directory`.red);
+  }
+};
